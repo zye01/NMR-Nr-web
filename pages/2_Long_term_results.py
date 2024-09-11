c4a4d0aae8e396fba9fb1031a89f3a5213c75464
zye306 <zye@gatech.edu>
2024-09-11 22:07:12 +0200

    Update 2_Long_term_results.py

--- pages/2_Long_term_results.py
+++ pages/2_Long_term_results.py
@@ -113,9 +113,10 @@ def plot_scatters(state,cm):
                 color=line_props[model]['color'],
                 width=2,
                 dash='dash'
-            )
             ),
             showlegend=False,
+            ),
+            
         )
 
     # add a 1:1 line
